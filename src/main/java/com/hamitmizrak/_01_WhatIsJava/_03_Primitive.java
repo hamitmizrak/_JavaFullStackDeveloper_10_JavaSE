c8a9773500d670672b09d7878cfcf5f3cd7a11c9
hamitmizrak <hamitmizrak@gmail.com>
2023-07-29 12:02:52 +0300

    Variable, operators

---
+++ src/main/java/com/hamitmizrak/_01_WhatIsJava/_03_Primitive.java
@@ -0,0 +1,35 @@
+package com.hamitmizrak._01_WhatIsJava;
+
+public class _03_Primitive {
+    public static void main(String[] args) {
+
+      // Sayılar
+        // byte, short, int ,long
+        byte b1=125;
+        short s1=125;
+        int i1=125;
+        long l1=125L;
+
+        float f2=15.55F;
+        double d2=14.53;
+
+        boolean b3=true;
+        char c3='a';
+
+        // operator
+        int number1=10,number2;
+        number2=2;
+        System.out.println("Toplama:"+(number1+number2));
+        System.out.println("Çıkarma:"+(number1-number2));
+        System.out.println("Çarp:"+(number1*number2));
+        System.out.println("Böl:"+(number1/number2));
+        System.out.println("Kalan:"+(number1%number2));
+
+        // &&=VE ||=VEYA !=DEĞİL
+
+        // atama  =
+        // eşittir ==
+
+        // 8 sayısı ile 3 sayısının 4 işlem yapan algoritma yazınız ?
+    }
+}
