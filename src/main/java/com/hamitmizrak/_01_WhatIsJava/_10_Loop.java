09d98736d8f198552bf94357cfdb9cbf48d01b30
hamitmizrak <hamitmizrak@gmail.com>
2023-07-29 15:39:15 +0300

    Loop

---
+++ src/main/java/com/hamitmizrak/_01_WhatIsJava/_10_Loop.java
@@ -0,0 +1,33 @@
+package com.hamitmizrak._01_WhatIsJava;
+
+public class _10_Loop {
+    public static void main(String[] args) {
+
+        // iterative For
+        // i++  , i=i+1  , i+=1
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+        // sonsuz döngü for
+        //for(;;){}
+
+        System.out.println("\n");
+        // while
+        int k = 1;
+        while (k <= 10) {
+            System.out.print(k + " ");
+            k++;
+        }
+        // while sonsuz döngü
+        // while(true){}
+        System.out.println("\n");
+
+        // while
+        int m = 1;
+        do {
+            System.out.print(m + " ");
+            m++;
+        }
+        while (m <= 10);
+    }
+}
