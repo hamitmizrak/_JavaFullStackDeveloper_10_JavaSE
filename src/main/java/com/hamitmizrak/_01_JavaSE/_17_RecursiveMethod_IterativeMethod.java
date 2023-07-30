ef2fcb18690373dd053d35846c18108867bcaa70
hamitmizrak <hamitmizrak@gmail.com>
2023-07-30 15:47:27 +0300

    Faktöriyel iterative, recursive

--- src/main/java/com/hamitmizrak/_01_JavaSE/_17_RecursiveMethod_IterativeMethod.java
+++ src/main/java/com/hamitmizrak/_01_JavaSE/_17_RecursiveMethod_IterativeMethod.java
@@ -11,6 +11,8 @@ import java.util.Scanner;
 // Java By Pass value mi ? (Kubilay)
 // decoder encoder araştırma ödevi ? (Faruk)
 // JAR-WAR  araştırma ödevi ? (Anıl)
+// String ters yazmak (StringReverse) ==>Girilen kelimeyi ters yazdıran algoritma ?
+// Palindrom (Metotla yapıyorsunuz)// Girilen bir kelimenin tersten okunuşu ve düzden okunuşu aynı ise AMA
 
 // GELECEK HAFTA: Diziler, başlıyoruz.
 // FILE
@@ -32,14 +34,14 @@ public class _17_RecursiveMethod_IterativeMethod {
     }
 
     // ITERATIVE
-    public int iterativeFactoriyel(int number) throws _0_HamitMizrak {
+    private int iterativeFactoriyel(int number) throws _0_HamitMizrak {
         int temp = 1; // unutma çarpmada 1 etkisiz elemandır.
 
         // Sıfırdan Küçük Girerse Exception Fırlatsın.
         if (number < 0) {
             //throw new _0_HamitMizrak("Faktöriyel için Sıfırdan Küçük Giremezsiniz");
             System.err.println("Negatif Sayı Giremezsiniz");
-            number=Math.abs(number);
+            number = Math.abs(number);
         }
         //Faktöriyel Sıfır veya Bir
         if (number == 0 || number == 1)
@@ -52,16 +54,55 @@ public class _17_RecursiveMethod_IterativeMethod {
         } //end else
     } //end method iterativeFactoriyel
 
+
+    // RECURSIVE
+    private int recursiveFaktoriyel(int number) throws _0_HamitMizrak {
+        int temp = 1; // unutma çarpmada 1 etkisiz elemandır.
+
+        // Sıfırdan Küçük Girerse Exception Fırlatsın.
+        if (number < 0) {
+            //throw new _0_HamitMizrak("Faktöriyel için Sıfırdan Küçük Giremezsiniz");
+            System.err.println("Negatif Sayı Giremezsiniz");
+            number = Math.abs(number);
+        }
+        //Faktöriyel Sıfır veya Bir
+        if (number == 0 || number == 1)
+            return 1;
+        else {
+            return number * recursiveFaktoriyel(number - 1);
+        } //end else
+    } //end method recursiveFaktoriyel
+
+    public void chooiseIterativeOrRecursive() throws _0_HamitMizrak {
+        for (; ; ) {
+            Scanner klavye = new Scanner(System.in);
+            System.out.println("\nLütfen Seçiniz:\n1-)Iterative Method\n2-)Recursive Method");
+            int chooise = klavye.nextInt();
+            switch (chooise) {
+                case 1:
+                    int number = useData();
+                    int result = iterativeFactoriyel(number);
+                    System.out.println(number+" sayısınını Iterative Sonucu: "+ result);
+                    break;
+
+                case 2:
+                    int number2 = useData();
+                    int result2 = iterativeFactoriyel(number2);
+                    System.out.println(number2+" sayısınını Recursive Sonucu: "+ result2);
+                    //System.out.println(recursiveFaktoriyel(useData()));
+                    break;
+
+                default:
+                    System.out.println("sadece 1 veya 2 seçilecektir.");
+                    break;
+            }
+        }
+    }
+
     public static void main(String[] args) throws _0_HamitMizrak {
         _17_RecursiveMethod_IterativeMethod method = new _17_RecursiveMethod_IterativeMethod();
-        for (;;) {
-            /*
-            int number = method.useData();
-            int result = method.iterativeFactoriyel(number);
-            System.out.println(result);
-            */
-
-            System.out.println( method.iterativeFactoriyel(method.useData()));
-        }
+        method.chooiseIterativeOrRecursive();
     }// end PSVM
+
+
 }// end Class
