c920097eb7bdfb660d7f3521b88214daabfaf3e8
hamitmizrak <hamitmizrak@gmail.com>
2023-07-30 14:51:48 +0300

    Faktöriyel

---
+++ src/main/java/com/hamitmizrak/_01_JavaSE/_17_RecursiveMethod_IterativeMethod.java
@@ -0,0 +1,21 @@
+package com.hamitmizrak._01_JavaSE;
+
+// Recursive,-iterative method
+// Faktöriyel iterative ve recursive yazınız ?
+// ENUM
+
+// ÖDEV: Asal sayı  yazınız ?
+// Java By Pass value mi ? (Kubilay)
+// decoder encoder araştırma ödevi ? (Faruk)
+// JAR-WAR  araştırma ödevi ? (Anıl)
+
+// GELECEK HAFTA: Diziler, başlıyoruz.
+// FILE
+// Thread
+// COLLECTION
+// OOP GİRİŞ, inheritance, abstract, interface
+// Java 8 gelen özellikler
+public class _17_RecursiveMethod_IterativeMethod {
+
+    // Kullanıcıdan alına bir sayıyı faktöriyeli hesaplayan java algoritmasını yazınız.
+}
