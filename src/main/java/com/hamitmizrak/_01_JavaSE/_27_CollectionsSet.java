aef90052a70696f1bfd8a2193907b2e5d090f7ab
Hamit Mızrak <hamitmizrak@gmail.com>
2023-08-12 18:39:53 +0300

    Collections Set

--- src/main/java/com/hamitmizrak/_01_JavaSE/_27_CollectionsSet.java
+++ src/main/java/com/hamitmizrak/_01_JavaSE/_27_CollectionsSet.java
@@ -4,29 +4,27 @@ package com.hamitmizrak._01_JavaSE;
 // OOP GİRİŞ, inheritance, abstract, interface
 // Java 8 gelen özellikler
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class _27_CollectionsSet {
 
     public static void main(String[] args) {
-        //List<Integer> listem=new LinkedList<>(); // araya yerleştirme , silme
-        List<Integer> listem=new ArrayList<>(); // ekleme ve arama
+
+        //Set<Integer> listem=new LinkedHashSet<>(); // eklediğiniz sırada
+        //Set<Integer> listem=new TreeSet<>(); // küçükten büyüğe doğru
+        Set<Integer> listem=new TreeSet<>(); // kafasına ekler
         listem.add(10);
         listem.add(20);
+        listem.add(30);
+        listem.add(30);
         listem.add(30);
         listem.add(5);
 
        /* System.out.println(listem.size());
-        System.out.println(listem.get(0));
         System.out.println(listem.get(listem.size()-1));
         System.out.println(listem.isEmpty());
         System.out.println(listem.hashCode());*/
 
-        for (int i = 0; i <listem.size() ; i++) {
-            System.out.println(listem.get(i));
-        }
-        listem.remove(0);
 
         System.out.println("*******************");
         for ( Integer temp  :listem) {
