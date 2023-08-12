d50f845de77a742fe146daeec45d5eca16bf7510
Hamit Mızrak <hamitmizrak@gmail.com>
2023-08-12 17:55:31 +0300

    File

--- src/main/java/com/hamitmizrak/_01_JavaSE/_23_File.java
+++ src/main/java/com/hamitmizrak/_01_JavaSE/_23_File.java
@@ -7,38 +7,73 @@ import java.util.Scanner;
 
 // FILE
 // Thread
-// COLLECTION
+// Generics
+// COLLECTION (List-set-map)
 // OOP GİRİŞ, inheritance, abstract, interface
 // Java 8 gelen özellikler
 
 public class _23_File {
+    public static final String URL = "C:\\io\\ecodation\\eco10.txt";
 
-    public static void main(String[] args) {
-        Scanner klavye=new Scanner(System.in);
+    private static String userData() {
+        Scanner klavye = new Scanner(System.in);
         System.out.println("lütfen birşey yazınız");
-        String userData=klavye.nextLine();
+        String userData = klavye.nextLine();
+        return userData;
+    }
 
+    private static void fileWriterUser(String userData) {
         //writer
-        try(BufferedWriter bufferedWriter=new BufferedWriter(new FileWriter("C:\\io\\ecodation\\eco10.txt",true))) {
-            bufferedWriter.write(userData+"\n");
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(_23_File.URL, true))) {
+            bufferedWriter.write(userData + "\n");
             bufferedWriter.flush();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
 
+    private static void fileReaderUser() {
         //reader
-        try(BufferedReader bufferedReader=new BufferedReader(new FileReader("C:\\io\\ecodation\\eco10.txt"))) {
-           StringBuilder stringBuilder=new StringBuilder();
-           String rows="";
-           while((rows=bufferedReader.readLine())!=null){
-               stringBuilder.append("\n").append(rows);
-           }
-           String data=stringBuilder.toString();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(_23_File.URL))) {
+            StringBuilder stringBuilder = new StringBuilder();
+            String rows = "";
+            while ((rows = bufferedReader.readLine()) != null) {
+                stringBuilder.append("\n").append(rows);
+            }
+            String data = stringBuilder.toString();
             System.out.println(data);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
 
+    public static void allMethod() {
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("\nSeçiniz.\n1-)Yazma\n2-)Okuma\n3-)Çıkış");
+            int chooise = scanner.nextInt();
+
+            switch (chooise) {
+                case 1:
+                    String user = userData();
+                    fileWriterUser(user);
+                    break;
+                case 2:
+                    fileReaderUser();
+                    break;
+                case 3:
+                    System.out.println("Sistemde Çıkış yapılıyor.");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Lütfen belirtilen aralıkta seçim yapınız");
+                    break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        allMethod();
     }
 
 }
