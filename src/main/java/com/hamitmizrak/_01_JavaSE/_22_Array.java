f4cb5953e23d1ff799bacda021233bc91bf51433
Hamit Mızrak <hamitmizrak@gmail.com>
2023-08-12 15:53:28 +0300

    Diziler matrix

--- src/main/java/com/hamitmizrak/_01_JavaSE/_22_Array.java
+++ src/main/java/com/hamitmizrak/_01_JavaSE/_22_Array.java
@@ -42,5 +42,24 @@ public class _22_Array {
             System.out.print(temp+" ");
         }
 
+        System.out.println("\n");
+        int[] [] matrix=new int[3][3];
+        matrix[0][0]=1;
+        matrix[0][1]=2;
+        matrix[0][2]=3;
+        matrix[1][0]=4;
+        matrix[1][1]=5;
+        matrix[1][2]=6;
+        matrix[2][0]=7;
+        matrix[2][1]=8;
+        matrix[2][2]=9;
+
+        for (int i = 0; i <matrix.length ; i++) {
+            for (int k = 0; k <matrix[i].length ; k++) {
+                System.out.print(matrix[i][k]+" ");
+            }
+            System.out.println();
+        }
+
     }
 }
